0191252e61d31c599424e4d4f4fb661e15bc50fd
vinhhocit04 <vinhadmw12@gmail.com>
2025-06-12 09:13:32 +0700

    up

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -17,7 +17,7 @@ public class GradeConverterTest {
     public void testConvert1() throws Exception {
         String expected = "A";
         String actual = converter.convert(100);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -62,6 +62,82 @@ public class GradeConverterTest {
         String actual = converter.convert(-10);
         assertEquals(expected,actual);
     }
+    @Test
+    public void testConvert9() throws Exception {
+        String expected = "Invalid";
+        String actual = converter.convert(-5);
+        assertEquals(expected, actual);
+    }
 
+    // Boundary value tests for grade ranges
+    @Test
+    public void testConvertBoundary89() throws Exception {
+        String expected = "B";
+        String actual = converter.convert(89);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary79() throws Exception {
+        String expected = "C";
+        String actual = converter.convert(79);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary69() throws Exception {
+        String expected = "D";
+        String actual = converter.convert(69);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary59() throws Exception {
+        String expected = "E";
+        String actual = converter.convert(59);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary0() throws Exception {
+        String expected = "E";
+        String actual = converter.convert(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary91() throws Exception {
+        String expected = "A";
+        String actual = converter.convert(91);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary81() throws Exception {
+        String expected = "B";
+        String actual = converter.convert(81);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary71() throws Exception {
+        String expected = "C";
+        String actual = converter.convert(71);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary61() throws Exception {
+        String expected = "D";
+        String actual = converter.convert(61);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertBoundary1() throws Exception {
+        String expected = "E";
+        String actual = converter.convert(1);
+        assertEquals(expected, actual);
+    }
 
 }
